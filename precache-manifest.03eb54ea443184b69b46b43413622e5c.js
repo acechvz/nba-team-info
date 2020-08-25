96d220d6fdfad17bcb7b8a787f08ffa487ec7e3a
Alan Ch <acechvz@gmail.com>
2020-08-25 02:09:49 -0500

    Updates

---
+++ precache-manifest.03eb54ea443184b69b46b43413622e5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "638bc55a0cb5230ca436f489e97d1703",
+    "url": "/index.html"
+  },
+  {
+    "revision": "940b017c87b7fdc4df60",
+    "url": "/static/css/main.d7080e58.chunk.css"
+  },
+  {
+    "revision": "384a45139b90c4fa0375",
+    "url": "/static/js/2.65db26d0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.65db26d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "940b017c87b7fdc4df60",
+    "url": "/static/js/main.afa6b471.chunk.js"
+  },
+  {
+    "revision": "df7996ee6219a1170413",
+    "url": "/static/js/runtime-main.23c3f2ef.js"
+  }
+]);
